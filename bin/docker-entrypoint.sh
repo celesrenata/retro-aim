44a61005faf8b5e6b2c54478ef074dd6ff2f240f
Celes Renata <celes@celestium.life>
2025-01-23 00:20:24 -0800

    add logging

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -6,5 +6,6 @@ if ! [ -f /app/data/settings.env ]; then
 	cp /usr/share/retro_aim_server/settings.env /app/data/settings.env
 fi
 
-/usr/bin/retro_aim_server -config /app/data/settings.env 2>&1 > /app/data/server.log
+/usr/bin/retro_aim_server -config /app/data/settings.env 2>&1 > /app/data/server.log &
+tail -f /app/data/server.log
 exec "$@"
